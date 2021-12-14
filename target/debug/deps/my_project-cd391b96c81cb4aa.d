f5078a71cfe9d2a9ca936303a2b57aa699992505
zafarluni <zafarluni@yahoo.com>
2021-12-14 18:53:36 +0000

    init

---
+++ target/debug/deps/my_project-cd391b96c81cb4aa.d
@@ -0,0 +1,5 @@
+/home/runner/Rust/target/debug/deps/my_project-cd391b96c81cb4aa.rmeta: src/main.rs
+
+/home/runner/Rust/target/debug/deps/my_project-cd391b96c81cb4aa.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/my_project-f5068bc5669dac34.d
@@ -0,0 +1,5 @@
+/home/runner/Rust/target/debug/deps/my_project-f5068bc5669dac34.rmeta: src/main.rs
+
+/home/runner/Rust/target/debug/deps/my_project-f5068bc5669dac34.d: src/main.rs
+
+src/main.rs:
